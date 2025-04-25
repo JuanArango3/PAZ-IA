4bd828cd6add610b0bcc34ee3923b302c84d9b94
Juan <jm.arango03@gmail.com>
2025-04-25 18:47:15 -0500

    Add Docker ignore file and update frontend service configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["pazia.jmarango.me"],
+        },
+    },
 };
 
 export default nextConfig;
